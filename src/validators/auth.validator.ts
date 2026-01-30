c091ee62e862974269d4422916b65dd0de54c641
Thomas Peligry <thomas.peligry@viacesi.fr>
2026-01-30 15:43:51 +0100

    validators for register and login schemas

---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8)
+    .regex(/[A-Z]/, "Le mot de passe requiert au moins une majuscule")
+    .regex(/[a-z]/, "Le mot de passe requiert au moins une minuscule")
+    .regex(/[0-9]/, "Le mot de passe requiert au moins un chiffre")
+    .regex(/[^A-Za-z0-9]/, "Le mot de passe requiert au moins un caractère spécial"),
+});
+
+export const loginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(1),
+});
