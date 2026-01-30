3429792fa362ce72f5988e0fa9c1e36c9e421068
Thomas Peligry <thomas.peligry@viacesi.fr>
2026-01-30 15:21:45 +0100

    created necessary files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import { Request, Response, NextFunction } from "express";
 
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+import authRoutes from "./routes/auth.route";
+import articleRoutes from "./routes/article.route";
+import exerciseRoutes from "./routes/exercise.route";
+
+app.use("/api/auth", authRoutes);
+app.use("/api/articles", articleRoutes);
+app.use("/api/exercises", exerciseRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Erreur interne du serveur" });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,4 +29,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+}) 
