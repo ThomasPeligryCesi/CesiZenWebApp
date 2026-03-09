22fe97fddd8d7877830c472523c7ee9d94eb2a33
Thomas Peligry <thomas.peligry@viacesi.fr>
2026-03-09 09:54:31 +0100

    (feat) added article related controller/route/service/validator

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -1,5 +1,18 @@
 import { Router } from "express";
-
+import { getAll, getById, create, update, remove, getFavorites, addFavorite, removeFavorite } from '../controllers/article.controller';
+import { authMiddleware } from "../middleware/auth.middleware";
+import { adminMiddleware } from "../middleware/admin.middleware";
 const router = Router();
+
+// Define routes for authentication
+router.get("/",authMiddleware, getAll);
+router.post("/", authMiddleware, adminMiddleware, create)
+router.put("/:id", authMiddleware, adminMiddleware, update)
+router.get("/favorites", authMiddleware, getFavorites)
+router.post("/favorites/:articleId", authMiddleware, addFavorite)
+router.delete("/favorites/:articleId", authMiddleware, removeFavorite)
+router.delete("/:id", authMiddleware, adminMiddleware, remove)
+router.get("/:id", authMiddleware, getById);
+
 
 export default router;
