e9a837246dc3160a5983de0b1cf91fe199a7be91
Thomas Peligry <thomas.peligry@viacesi.fr>
2026-03-09 09:55:20 +0100

    (feat) added auth admin related controller/route/middleware/service

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,24 @@
+import {Request, Response, NextFunction} from 'express';
+import {verify} from 'jsonwebtoken';
+ export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+
+    const authorization = req.headers.authorization;
+    const token = authorization?.slice("Bearer ".length).trim();
+    if (!authorization || !token) {
+        return res.status(401).json({message: 'No token provided'});
+    }
+    try {
+        const decoded = verify(token, process.env.JWT_SECRET as string);
+        if(typeof decoded === 'string') {
+            return res.status(401).json({
+                message: 'Invalid token',
+                success: false
+            });
+        }
+        res.locals.userId = decoded.userId;
+        res.locals.role = decoded.role;
+        next();
+    } catch (error) {
+        return res.status(401).json({message: 'Invalid token'});
+    }
+}
